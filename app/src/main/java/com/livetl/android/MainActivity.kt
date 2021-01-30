be4f5aa9cbb881d0bf733291f776aee968c9fea7
arkon <eugcheung94@gmail.com>
2021-01-29 23:04:34 -0500

    Remove Material components dependency, use proper icon

--- app/src/main/java/com/livetl/android/MainActivity.kt
+++ app/src/main/java/com/livetl/android/MainActivity.kt
@@ -2,10 +2,12 @@ package com.livetl.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import com.livetl.android.ui.theme.LiveTLTheme
@@ -15,12 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LiveTLTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -33,6 +43,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     LiveTLTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
