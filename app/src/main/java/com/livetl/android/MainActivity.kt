ab4959d624cdf37ad23a85002855278d5e96c99b
arkon <eugcheung94@gmail.com>
2021-01-29 23:05:52 -0500

    Move composables out of MainActivity

--- app/src/main/java/com/livetl/android/MainActivity.kt
+++ app/src/main/java/com/livetl/android/MainActivity.kt
@@ -2,14 +2,8 @@ package com.livetl.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
+import com.livetl.android.screens.MainScreen
 import com.livetl.android.ui.theme.LiveTLTheme
 
 class MainActivity : AppCompatActivity() {
@@ -20,29 +14,5 @@ class MainActivity : AppCompatActivity() {
                 MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun MainScreen() {
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LiveTLTheme {
-        MainScreen()
     }
 }
