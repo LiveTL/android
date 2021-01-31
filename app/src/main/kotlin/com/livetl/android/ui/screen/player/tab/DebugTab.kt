f76afb48451d7755f93094b7872c419e3d2eb11a
arkon <eugcheung94@gmail.com>
2021-01-31 16:13:07 -0500

    Add way to manually input a YT URL or ID for testing

--- app/src/main/kotlin/com/livetl/android/ui/screen/player/tab/DebugTab.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/player/tab/DebugTab.kt
@@ -3,12 +3,19 @@ package com.livetl.android.ui.screen.player.tab
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun DebugTab(
     setSource: (String) -> Unit
 ) {
+    var customUrlOrId by remember { mutableStateOf("") }
+
     Row {
         Button(onClick = { setSource("https://www.youtube.com/watch?v=nJgjiil5lz8") }) {
             Text("Sample 1")
@@ -20,4 +27,13 @@ fun DebugTab(
             Text("Sample 3")
         }
     }
+
+    TextField(
+        label = { Text("YouTube URL or ID") },
+        value = customUrlOrId,
+        onValueChange = {
+            customUrlOrId = it
+            setSource(it)
+        },
+    )
 }
