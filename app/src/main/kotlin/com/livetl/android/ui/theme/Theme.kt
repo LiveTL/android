b80fa88e2a6b41ad2700c443cce18af333be2ba4
arkon <eugcheung94@gmail.com>
2021-01-31 11:11:47 -0500

    Remove light theme

--- app/src/main/kotlin/com/livetl/android/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/livetl/android/ui/theme/Theme.kt
@@ -1,9 +1,7 @@
 package com.livetl.android.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
@@ -12,31 +10,10 @@ private val DarkColorPalette = darkColors(
         secondary = teal200
 )
 
-private val LightColorPalette = lightColors(
-        primary = purple500,
-        primaryVariant = purple700,
-        secondary = teal200
-
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
 @Composable
-fun LiveTLTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun LiveTLTheme(content: @Composable() () -> Unit) {
     MaterialTheme(
-            colors = colors,
+            colors = DarkColorPalette,
             typography = typography,
             shapes = shapes,
             content = content
