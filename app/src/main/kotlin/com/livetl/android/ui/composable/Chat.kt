baf70b5c078fef61b3b0b68aa6d7d41d912b4031
arkon <eugcheung94@gmail.com>
2021-02-14 17:57:23 -0500

    Placeholder chat messages

--- app/src/main/kotlin/com/livetl/android/ui/composable/Chat.kt
+++ app/src/main/kotlin/com/livetl/android/ui/composable/Chat.kt
@@ -1,15 +1,18 @@
 package com.livetl.android.ui.composable
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun Chat(modifier: Modifier = Modifier) {
-//    val messages = (1..200).map { "Message #$it" }
-//
-//    LazyColumn {
-//        items(messages) { message ->
-//            Text(text = message)
-//        }
-//    }
+    val messages = (1..200).map { "Message #$it" }
+
+    LazyColumn(modifier = modifier) {
+        items(messages) { message ->
+            Text(message)
+        }
+    }
 }
