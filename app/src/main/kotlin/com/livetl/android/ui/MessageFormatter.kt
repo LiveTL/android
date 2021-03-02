398b9135a21bcede2dbdf1695157375ed89cbe77
arkon <eugcheung94@gmail.com>
2021-03-01 22:27:51 -0500

    Handle links in stream info

---
+++ app/src/main/kotlin/com/livetl/android/ui/MessageFormatter.kt
@@ -0,0 +1,94 @@
+package com.livetl.android.ui
+
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+
+// Regex containing the syntax tokens
+val symbolPattern by lazy {
+    Regex("""(https?://[^\s\t\n]+)""")
+}
+
+// Accepted annotations for the ClickableTextWrapper
+enum class SymbolAnnotationType {
+    LINK
+}
+typealias StringAnnotation = AnnotatedString.Range<String>
+// Pair returning styled content and annotation for ClickableText when matching syntax token
+typealias SymbolAnnotation = Pair<AnnotatedString, StringAnnotation?>
+
+/**
+ * Format a message following Markdown-lite syntax
+ * | @username -> bold, primary color and clickable element
+ * | http(s)://... -> clickable link, opening it into the browser
+ * | *bold* -> bold
+ * | _italic_ -> italic
+ * | ~strikethrough~ -> strikethrough
+ * | `MyClass.myMethod` -> inline code styling
+ *
+ * @param text contains message to be parsed
+ * @return AnnotatedString with annotations used inside the ClickableText wrapper
+ */
+@Composable
+fun messageFormatter(text: String): AnnotatedString {
+    val tokens = symbolPattern.findAll(text)
+
+    return buildAnnotatedString {
+        var cursorPosition = 0
+
+        for (token in tokens) {
+            append(text.slice(cursorPosition until token.range.first))
+
+            val (annotatedString, stringAnnotation) = getSymbolAnnotation(
+                matchResult = token,
+                colors = MaterialTheme.colors,
+            )
+            append(annotatedString)
+
+            if (stringAnnotation != null) {
+                val (item, start, end, tag) = stringAnnotation
+                addStringAnnotation(tag = tag, start = start, end = end, annotation = item)
+            }
+
+            cursorPosition = token.range.last + 1
+        }
+
+        if (!tokens.none()) {
+            append(text.slice(cursorPosition..text.lastIndex))
+        } else {
+            append(text)
+        }
+    }
+}
+
+/**
+ * Map regex matches found in a message with supported syntax symbols
+ *
+ * @param matchResult is a regex result matching our syntax symbols
+ * @return pair of AnnotatedString with annotation (optional) used inside the ClickableText wrapper
+ */
+private fun getSymbolAnnotation(
+    matchResult: MatchResult,
+    colors: Colors,
+): SymbolAnnotation {
+    return when (matchResult.value.first()) {
+        'h' -> SymbolAnnotation(
+            AnnotatedString(
+                text = matchResult.value,
+                spanStyle = SpanStyle(
+                    color = colors.primary
+                )
+            ),
+            StringAnnotation(
+                item = matchResult.value,
+                start = matchResult.range.first,
+                end = matchResult.range.last,
+                tag = SymbolAnnotationType.LINK.name
+            )
+        )
+        else -> SymbolAnnotation(AnnotatedString(matchResult.value), null)
+    }
+}
--- app/src/main/kotlin/com/livetl/android/ui/screen/player/section/InfoTab.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/player/section/InfoTab.kt
@@ -6,23 +6,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import com.livetl.android.data.stream.StreamInfo
+import com.livetl.android.ui.SymbolAnnotationType
+import com.livetl.android.ui.messageFormatter
 
 @Composable
 fun InfoTab(
     streamInfo: StreamInfo?
 ) {
+    val uriHandler = LocalUriHandler.current
+
     if (streamInfo != null) {
         Column(
             modifier = Modifier
@@ -41,7 +47,23 @@ fun InfoTab(
             Spacer(modifier = Modifier.requiredHeight(8.dp))
             Divider()
             Spacer(modifier = Modifier.requiredHeight(8.dp))
-            Text(text = streamInfo.shortDescription)
+
+            val styledDescription = messageFormatter(streamInfo.shortDescription)
+            ClickableText(
+                text = styledDescription,
+                style = MaterialTheme.typography.body1.copy(color = LocalContentColor.current),
+                onClick = {
+                    styledDescription
+                        .getStringAnnotations(start = it, end = it)
+                        .firstOrNull()
+                        ?.let { annotation ->
+                            when (annotation.tag) {
+                                SymbolAnnotationType.LINK.name -> uriHandler.openUri(annotation.item)
+                                else -> Unit
+                            }
+                        }
+                }
+            )
         }
     } else {
         Box(
