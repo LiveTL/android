c01f4321f55511ef9651edebb94bbbff2b3abce1
arkon <eugcheung94@gmail.com>
2021-03-03 22:31:52 -0500

    Use blue for accent color

--- app/src/main/kotlin/com/livetl/android/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/livetl/android/ui/theme/Theme.kt
@@ -3,15 +3,16 @@ package com.livetl.android.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
+    primary = Color(0xFF0094F7),
+    primaryVariant = Color(0xFF004D81),
+    secondary = Color(0xFF4CAF50),
 )
 
 @Composable
-fun LiveTLTheme(content: @Composable() () -> Unit) {
+fun LiveTLTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = DarkColorPalette,
         typography = typography,
