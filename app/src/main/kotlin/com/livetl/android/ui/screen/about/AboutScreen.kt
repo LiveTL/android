3e6a90209305be359ba0b8bc41b006d63903faec
arkon <eugcheung94@gmail.com>
2021-03-13 11:20:36 -0500

    Add some stuff to the About screen

--- app/src/main/kotlin/com/livetl/android/ui/screen/about/AboutScreen.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/about/AboutScreen.kt
@@ -1,6 +1,11 @@
 package com.livetl.android.ui.screen.about
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -8,8 +13,17 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material.icons.filled.Savings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.livetl.android.BuildConfig
 import com.livetl.android.R
 
@@ -34,9 +48,65 @@ fun AboutScreen(
             )
         }
     ) {
-        Column {
-            Text(BuildConfig.APPLICATION_ID)
-            Text(BuildConfig.VERSION_NAME)
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            /* logo */
+
+            Text(stringResource(R.string.about_version, BuildConfig.VERSION_NAME))
+
+            Row {
+                LinkIcon(
+                    labelRes = R.string.about_website,
+                    url = "https://livetl.app/",
+                    icon = Icons.Filled.Public,
+                )
+                LinkIcon(
+                    labelRes = R.string.about_github,
+                    url = "https://github.com/LiveTL/android",
+                    icon = Icons.Filled.Code,
+                )
+                LinkIcon(
+                    labelRes = R.string.about_donate,
+                    url = "https://opencollective.com/livetl",
+                    icon = Icons.Filled.Savings,
+                )
+            }
+
+            /*
+            Credits
+            Holotools stream info
+            OSS Licenses
+             */
         }
     }
+}
+
+@Composable
+private fun LinkIcon(
+    @StringRes labelRes: Int,
+    url: String,
+    icon: ImageVector,
+) {
+    val uriHandler = LocalUriHandler.current
+
+    Column(
+        modifier = Modifier
+            .clickable { uriHandler.openUri(url) }
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Text(stringResource(labelRes))
+    }
+}
+
+@Preview
+@Composable
+private fun AboutScreenPreview() {
+    AboutScreen(onBackPressed = {})
 }
