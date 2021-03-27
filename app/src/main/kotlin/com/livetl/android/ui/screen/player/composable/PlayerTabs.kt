965abb011f8aeca0bb952890dfb566704679f2d0
arkon <eugcheung94@gmail.com>
2021-03-27 14:45:20 -0400

    Use HorizontalPager from Accompanist library

--- app/src/main/kotlin/com/livetl/android/ui/screen/player/composable/PlayerTabs.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/player/composable/PlayerTabs.kt
@@ -1,20 +1,36 @@
 package com.livetl.android.ui.screen.player.composable
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Tab
+import androidx.compose.material.TabPosition
 import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.util.fastForEachIndexed
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
 import com.livetl.android.R
 import com.livetl.android.data.chat.ChatService
 import com.livetl.android.data.stream.StreamInfo
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.get
 
 enum class Tabs(@StringRes val nameRes: Int) {
@@ -24,29 +40,78 @@ enum class Tabs(@StringRes val nameRes: Int) {
 }
 val tabs = Tabs.values().toList()
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun PlayerTabs(
     streamInfo: StreamInfo?,
     chatService: ChatService = get(),
 ) {
     val chatMessages by chatService.messages.collectAsState()
-    var selectedTab by remember { mutableStateOf(Tabs.Info) }
 
-    TabRow(selectedTabIndex = selectedTab.ordinal) {
-        tabs.fastForEachIndexed { index, tab ->
-            Tab(
-                text = { Text(stringResource(tab.nameRes)) },
-                selected = index == selectedTab.ordinal,
-                onClick = { selectedTab = tab }
-            )
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState(pageCount = tabs.size)
+
+    Column(Modifier.fillMaxSize()) {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            tabs.fastForEachIndexed { index, tab ->
+                Tab(
+                    text = { Text(stringResource(tab.nameRes)) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.Top,
+        ) { page ->
+            when (page) {
+                Tabs.Info.ordinal -> InfoTab(streamInfo = streamInfo)
+                Tabs.Chat.ordinal -> Chat(
+                    messages = chatMessages,
+                    showJumpToBottomButton = true,
+                )
+                Tabs.Settings.ordinal -> SettingsTab()
+            }
         }
     }
-    when (selectedTab) {
-        Tabs.Info -> InfoTab(streamInfo = streamInfo)
-        Tabs.Chat -> Chat(
-            messages = chatMessages,
-            showJumpToBottomButton = true,
-        )
-        Tabs.Settings -> SettingsTab()
+}
+
+@OptIn(ExperimentalPagerApi::class)
+fun Modifier.pagerTabIndicatorOffset(
+    pagerState: PagerState,
+    tabPositions: List<TabPosition>,
+): Modifier = composed {
+    val targetIndicatorOffset: Dp
+    val indicatorWidth: Dp
+
+    val currentTab = tabPositions[pagerState.currentPage]
+    val nextTab = tabPositions.getOrNull(pagerState.currentPage + 1)
+    if (nextTab != null) {
+        // If we have a next tab, lerp between the size and offset
+        targetIndicatorOffset = lerp(currentTab.left, nextTab.left, pagerState.currentPageOffset)
+        indicatorWidth = lerp(currentTab.width, nextTab.width, pagerState.currentPageOffset)
+    } else {
+        // Otherwise we just use the current tab/page
+        targetIndicatorOffset = currentTab.left
+        indicatorWidth = currentTab.width
     }
+
+    fillMaxWidth()
+        .wrapContentSize(Alignment.BottomStart)
+        .offset(x = targetIndicatorOffset)
+        .width(indicatorWidth)
 }
