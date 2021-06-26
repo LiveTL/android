9b7adb1eacf61214b0144142728dce4a049dd0d4
arkon <eugcheung94@gmail.com>
2021-06-26 16:12:02 -0400

    Filter out upcoming streams not within a week

--- app/src/main/kotlin/com/livetl/android/data/feed/FeedService.kt
+++ app/src/main/kotlin/com/livetl/android/data/feed/FeedService.kt
@@ -1,5 +1,6 @@
 package com.livetl.android.data.feed
 
+import com.livetl.android.util.toDate
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
@@ -8,13 +9,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.Date
 import javax.inject.Inject
 
 class FeedService @Inject constructor(private val client: HttpClient, private val json: Json) {
 
     suspend fun getFeed(): Feed = withContext(Dispatchers.IO) {
         val result = client.get<HttpResponse>(SCHEDULE_API)
-        json.decodeFromString(result.readText())
+        val feed: Feed = json.decodeFromString(result.readText())
+
+        // Filter out streams that are scheduled for more than a week from now
+        // (usually things like free chats)
+        val fallbackDate = Date.from(Instant.now())
+        val weekLimit = Date.from(Instant.now().plus(7, ChronoUnit.DAYS))
+        feed.copy(
+            upcoming = feed.upcoming.filter {
+                val scheduledStart = it.live_schedule?.toDate() ?: fallbackDate
+                scheduledStart <= weekLimit
+            }
+        )
     }
 }
 
