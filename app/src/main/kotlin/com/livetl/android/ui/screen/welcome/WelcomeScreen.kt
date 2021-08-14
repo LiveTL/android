552cf02d4664e378767400829017d069862b7c3d
arkon <eugcheung94@gmail.com>
2021-08-14 12:16:54 -0400

    Replace welcome screen contents (closes #26)

--- app/src/main/kotlin/com/livetl/android/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/welcome/WelcomeScreen.kt
@@ -1,47 +1,32 @@
 package com.livetl.android.ui.screen.welcome
 
-import android.webkit.WebView
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.google.accompanist.insets.ui.Scaffold
 import com.google.accompanist.insets.ui.TopAppBar
 import com.livetl.android.R
-import com.livetl.android.util.setDefaultSettings
 
 @Composable
 fun WelcomeScreen(
     navigateToHome: () -> Unit,
     viewModel: WelcomeViewModel,
 ) {
-    val context = LocalContext.current
-
-    val webview = remember {
-        WebView(context).apply {
-            setDefaultSettings()
-            loadUrl("file:///android_asset/welcome.html")
-        }
-    }
-
-    DisposableEffect(Unit) {
-        onDispose {
-            webview.destroy()
-        }
-    }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -68,11 +53,51 @@ fun WelcomeScreen(
             )
         },
     ) { contentPadding ->
-        AndroidView(
-            factory = { webview },
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(contentPadding)
-        )
+        Box(Modifier.padding(contentPadding)) {
+            LazyColumn(Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                item {
+                    Text(
+                        text = stringResource(R.string.welcome_thank_you),
+                        style = MaterialTheme.typography.h5,
+                    )
+                }
+
+                item {
+                    Text(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        text = stringResource(R.string.welcome_get_started),
+                    )
+                }
+
+                item {
+                    Divider(Modifier.padding(8.dp))
+                }
+
+                item {
+                    Text(
+                        text = stringResource(R.string.welcome_faq),
+                        style = MaterialTheme.typography.h5,
+                    )
+                }
+
+                items(FAQ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        text = stringResource(it.first),
+                        fontWeight = FontWeight.W700,
+                    )
+                    Text(
+                        text = stringResource(it.second),
+                    )
+                }
+            }
+        }
     }
 }
+
+private val FAQ = listOf(
+    R.string.welcome_faq_q_how_it_works to R.string.welcome_faq_a_how_it_works,
+    R.string.welcome_faq_q_trust to R.string.welcome_faq_a_trust,
+    R.string.welcome_faq_q_no_translations to R.string.welcome_faq_a_no_translations,
+    R.string.welcome_faq_q_custom_filter to R.string.welcome_faq_a_custom_filter,
+)
