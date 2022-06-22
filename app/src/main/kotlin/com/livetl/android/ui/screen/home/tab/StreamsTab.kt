826607dd6a5eff7d99df719f70e7abda75fdd28c
arkon <eugcheung94@gmail.com>
2022-06-22 18:04:10 -0400

    Show message if streams tab is empty

--- app/src/main/kotlin/com/livetl/android/ui/screen/home/tab/StreamsTab.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/home/tab/StreamsTab.kt
@@ -1,16 +1,21 @@
 package com.livetl.android.ui.screen.home.tab
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.livetl.android.R
 import com.livetl.android.data.feed.Stream
 import com.livetl.android.data.feed.StreamStatus
 import com.livetl.android.ui.screen.home.composable.Stream
@@ -47,6 +52,16 @@ fun StreamsTab(
         state = refreshingFeed,
         onRefresh = { refreshFeed() },
     ) {
+        if (!refreshingFeed.isRefreshing && viewModel.streams.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(stringResource(R.string.empty_streams))
+            }
+            return@SwipeRefresh
+        }
+
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
         ) {
