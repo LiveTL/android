5d6e4ff5bd553b8fe33fa4ea03620f965e750bcf
arkon <arkon@users.noreply.github.com>
2024-05-25 16:49:00 -0400

    Indicate live stream in info panel

--- app/src/main/kotlin/com/livetl/android/ui/screen/player/composable/StreamInfoPanel.kt
+++ app/src/main/kotlin/com/livetl/android/ui/screen/player/composable/StreamInfoPanel.kt
@@ -1,15 +1,22 @@
 package com.livetl.android.ui.screen.player.composable
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.livetl.android.R
 import com.livetl.android.data.stream.StreamInfo
 import com.livetl.android.ui.common.StreamThumbnailBackground
 
@@ -36,10 +43,25 @@ fun StreamInfoPanel(streamInfo: StreamInfo?, modifier: Modifier = Modifier) {
                 style = MaterialTheme.typography.titleLarge,
             )
 
-            Text(
-                text = streamInfo.author,
-                style = MaterialTheme.typography.bodyMedium,
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                Text(
+                    text = streamInfo.author,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+
+                if (streamInfo.isLive) {
+                    Text(
+                        text = stringResource(R.string.live).uppercase(),
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(4.dp))
+                            .background(color = Color(0xE6CC0000))
+                            .padding(horizontal = 2.dp),
+                    )
+                }
+            }
         }
     }
 }
